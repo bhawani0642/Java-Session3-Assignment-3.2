10c66e1406adaa82627ff1e4084798950095ca82
bhawani0642 <bhawani0642@gmail.com>
2017-06-05 15:12:57 +0530

    Addition of two numbers without using + operator

---
+++ src/withoutOperator/Adittion.java
@@ -0,0 +1,11 @@
+package withoutOperator;
+
+public class Adittion {
+	public static void main(String[]args){
+		int x=8;
+		int y=9;
+		int z=x-(-y);
+		System.out.println("Answer is ="+z);
+	}
+
+}
